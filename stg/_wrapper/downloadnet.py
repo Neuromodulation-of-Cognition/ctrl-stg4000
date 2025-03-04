4f5bdaccd3aaf2d2bf4f87d3c3fb2fc29d932e4c
syntheticdinosaur <syntheticdinosaur>
2025-03-04 17:29:24 +0100

    added some type checking and conversion

--- stg/_wrapper/downloadnet.py
+++ stg/_wrapper/downloadnet.py
@@ -15,7 +15,7 @@ from stg._wrapper.dll import (
 
 class STG4000(STGX):
     """
-    This class implements the interface to download, start and stop stimulation. 
+    This class implements the interface to download, start and stop stimulation.
 
     At this point, you should pay attention to two specific details. First, indexing starts at 0. That means, the first channel is channel 0. Second, there is a difference between channels and triggers for the STG. Triggers are mapped to channels according to a channelmap. That means a single trigger can start stimulation of a whole set of channels. During initialization of the STG, we give this a sensible default. That means, all triggers are mapped to the respective channels following diagonal identity, i.e. trigger 0 maps to channel 0. Use :meth:`~STG4000.diagonalize_triggermap` to repeat this normalization.
 
@@ -27,33 +27,33 @@ class STG4000(STGX):
 
         import time
         from stg.api import STG4000
-        
+
         stg = STG4000()
         stg.download(0,[1,-1, 0], [0.1, 0.1, 49.8])
         while True:
-            time.sleep(0.5)    
+            time.sleep(0.5)
             a.trigger()
             stg.start_stimulation([0])
 
     .. note::
-       
+
        * Indexing starts at zero
        * Differentiate triggers and channels
 
     .. warning::
-        
-       Please note, that in download mode, the Python object and the STG do not share states. More specifically, once a program is downloaded to the STG, it stays downloaded, even if you delete the object, instantiate a new one or reboot the PC. You can enforce a clean state by rebooting the STG or explicitly downloading and thereby overwriting the channels after instantiation. This behavior was kept for three reasons. First, this is also the behavior of the MCS GUI, and therefore less non-intuitive if you come from this direction. Second, it clearly shows that in download mode, PC and STG are not coupled, i.e. once downloaded, you can trigger without any USB connection. Third, clearing the downloaded programs during instantiation (or deletion of the object) would prevent the user from common use cases like recovering the STG after a restart of the kernel, deletion of the object or unplugging the USB cable. 
+
+       Please note, that in download mode, the Python object and the STG do not share states. More specifically, once a program is downloaded to the STG, it stays downloaded, even if you delete the object, instantiate a new one or reboot the PC. You can enforce a clean state by rebooting the STG or explicitly downloading and thereby overwriting the channels after instantiation. This behavior was kept for three reasons. First, this is also the behavior of the MCS GUI, and therefore less non-intuitive if you come from this direction. Second, it clearly shows that in download mode, PC and STG are not coupled, i.e. once downloaded, you can trigger without any USB connection. Third, clearing the downloaded programs during instantiation (or deletion of the object) would prevent the user from common use cases like recovering the STG after a restart of the kernel, deletion of the object or unplugging the USB cable.
 
     """
 
     def stop_stimulation(self, triggerIndex: List[int] = []):
-        """stops all trigger inputs or a selection based on a list 
-        
+        """stops all trigger inputs or a selection based on a list
+
         args
         ----
         triggerIndex:List[int]
-            defaults to [], which stops stimulation at all channels. Give it a list of integers to start a specific subset of triggers, e.g. [0,1].   
-        
+            defaults to [], which stops stimulation at all channels. Give it a list of integers to start a specific subset of triggers, e.g. [0,1].
+
         """
 
         if triggerIndex == []:
@@ -62,35 +62,47 @@ class STG4000(STGX):
             interface.SendStop(System.UInt32(bitmap(triggerIndex)))
 
     def start_stimulation(self, triggerIndex: List[int] = []):
-        """starts all trigger inputs or a selection based on a list 
-        
+        """starts all trigger inputs or a selection based on a list
+
         args
         ----
         triggerIndex:List[int]
-            defaults to [], which starts stimulation at all channels. Give it a list of integers to start a specific subset of triggers, e.g. [0,1].         
-        
+            defaults to [], which starts stimulation at all channels. Give it a list of integers to start a specific subset of triggers, e.g. [0,1].
+
         """
+        if isinstance(triggerIndex, float):
+            raise ValueError(f"{triggerIndex=}, should be list[int] or int.")
+
+        if isinstance(triggerIndex, int):
+            triggerIndex = [triggerIndex]
 
         if triggerIndex == []:
             triggerIndex = [c for c in range(self.channel_count)]
+
+        for idx, v in enumerate(triggerIndex):
+            if not isinstance(v, int):
+                raise ValueError(
+                    f"{triggerIndex=} should be list[int], but {v} at {idx=} was {type(v)}."
+                )
+
         with self.interface() as interface:
             interface.SendStart(System.UInt32(bitmap(triggerIndex)))
 
     def set_mode(self, channel_index: List[int] = [], mode: str = "current") -> int:
         """set a single or all channels to voltage or current mode
-        
+
         args
         ----
-        channel_index: list        
+        channel_index: list
             defaults to [], which sets the mode at all channels. Give it a list of integers to set the mode only for a specific subset of channels, e.g. [0,1].
         mode: str ("current", "voltage")
             defaults to current
 
 
         .. warning::
-           
+
            Because we primarily use current-mode, voltage mode is relatively untested. Additionally, i so far have not tested the behavior when different channels are in different modes. Be safe, and just set all channels to current-mode with :code:`stg.set_mode("current")`
-        
+
 
         """
         if mode == "current":
@@ -106,7 +118,7 @@ class STG4000(STGX):
 
     def diagonalize_triggermap(self):
         """Give each trigger a sensible channel
-                       
+
         Use this function to normalize the mapping of trigger to channel to a diagonal identity, i.e. trigger 0 maps to channel 0,  so on.
 
             +----------+---+---+---+---+---+---+---+---+
@@ -114,7 +126,7 @@ class STG4000(STGX):
             +----------+---+---+---+---+---+---+---+---+
             | Channel  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
             +----------+---+---+---+---+---+---+---+---+
-        
+
         """
         channelmap = []
         syncoutmap = []
@@ -133,14 +145,13 @@ class STG4000(STGX):
         durations_in_ms: List[float,] = [0],
         mode="current",
     ):
+        """Download a stimulation signal
 
-        """Download a stimulation signal 
-        
         .. Warning::
 
            Any previous data sent to that channel is erased. Other channels stay untouched.
-           
-        The signal is compressed as amplitudes and their respective durations 
+
+        The signal is compressed as amplitudes and their respective durations
 
         args
         ----
@@ -148,23 +159,23 @@ class STG4000(STGX):
             The index of the channel for which to download the signal. Indexing
             starts at 0
         amplitudes_in_mA: List[float]
-            a list of amplitudes in mA/mV delivered for the corresponding duration 
+            a list of amplitudes in mA/mV delivered for the corresponding duration
         durations_in_ms: List[float]
-            a list of durations in ms determing how long each corresponding 
+            a list of durations in ms determing how long each corresponding
             amplitude is delivered
         mode: str
             defaults to current
 
         Example
         -------
-     
+
         .. code-block:: python
-            
+
            stg.download(channel_index = 0,
                         amplitudes_in_mA = [1, -1, 0],
                         durations_in_ms = [.1, .1, .488])
-           
-    
+
+
         """
         if len(amplitudes_in_mA) != len(durations_in_ms):
             raise ValueError("Every amplitude needs a duration and vice versa!")
